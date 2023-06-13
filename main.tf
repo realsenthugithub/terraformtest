eb514e7185aa56fb028f3a224d11f3e69daadce1
realsenthugithub <realsenthu.github@gmail.com>
2023-06-13 16:18:25 +0800

    ec2

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-output "name" {
-    value = "senthu"
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
 }
