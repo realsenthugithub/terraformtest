76ea0616cb1643e8258579952975d341bd3acf8e
realsenthugithub <realsenthu.github@gmail.com>
2023-06-14 10:31:51 +0800

    cool

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-/* resource "aws_instance" "example" {
+resource "aws_instance" "example" {
   ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
-} */
+} 
